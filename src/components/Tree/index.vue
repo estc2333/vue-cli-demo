02b06ecc7e9b85b630008007ed06dee6cd1dd9b2
yang yang <linran2014@outlook.com>
2022-03-21 17:20:29 +0800

    [feat:] add tree

---
+++ src/components/Tree/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-if="treeData.length">
+    <div v-for="node in treeData" :key="node.label">
+      <span>{{node.label}}</span>
+      <ul>
+        <Tree :treeData="node.children" :key="node.label" />
+      </ul>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "Tree",
+  props: {
+    treeData: {
+      type: {
+        Array,
+        default: () => ([])
+      }
+    },
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Agent/index.vue
+++ src/views/Agent/index.vue
@@ -1,10 +1,48 @@
 <template>
-  <div>agent</div>
+  <div>agent
+    <Tree v-for="node in treeData" :key="node.label" :treeData="treeData" />
+  </div>
 </template>
 
 <script>
+import Tree from '../../components/Tree'
 export default {
-  name: "agent"
+  name: "agent",
+  components: {
+    Tree,
+  },
+  data () {
+    return {
+      treeData: [
+        {
+          label: 'Level1 a',
+          children: [
+            {
+              label: 'Level2 a',
+              children: [
+                {
+                  label: 'Level3 a',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          label: 'Level1 b',
+          children: [
+            {
+              label: 'Level2 b',
+              children: [
+                {
+                  label: 'Level3 b',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    }
+  },
 }
 </script>
 
